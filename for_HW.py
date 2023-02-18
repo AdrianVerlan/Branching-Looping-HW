4cf96709d04c702d1f9f0a54ce4b74a340960afe
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-02-18 11:18:28 +0200

    For hw updated
    A fost adaugata functia reversed pentru a face programul sa numere invers

--- for_HW.py
+++ for_HW.py
@@ -2,4 +2,10 @@ from time import sleep
 for n in range(1,11):
     sleep(0.2)
     print (n)
-
+if n == 10:
+    print("Now Reverse")
+for n in reversed(range(1,11)):
+    sleep(0.2)
+    print (n)
+if n == 1:
+    print("Finish")
