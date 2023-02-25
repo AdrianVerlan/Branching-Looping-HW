5a117f9b45ed3df48afeb5b9676582e11f941ae6
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-02-25 11:40:47 +0200

    Updated with all subfolders

--- file_system_structure.py
+++ file_system_structure.py
@@ -2,19 +2,44 @@ from os import system
 system("cls")
 disk = ["home","system","config","backup"]
 home_folder= ["pictures","music","docs"]
+system_folder =["windows", "nvidia","adobe"]
+config_folder =["temp","drivers","premiere"]
+backup_folder =["system bk","home bk","config bk"]
 print("/-.")
 for i in range(4):
     suf=""
     if disk[i] == "home":
-        suf == " -."
+        suf = " -."
     print ("  +-- "+ disk[i]+suf)
     if disk[i]=="home":
         for j in range(3):
-            
+            suf=""
             print("  |         +-- "+home_folder[j])
             if j == 2: break
             print("  |         |")
+
+    if disk[i]=="system":
+        suf = " -."
+        for k in range(3):
+            print("  |           +-- "+system_folder[k])
+            if k == 2: break
+            print("  |           |")
+    
+        
+    if disk[i]=="config":
+        suf = " -."
+        for l in range(3):                  
+            print("  |           +-- "+config_folder[l])
+            if l == 2: break
+            print("  |           |")
+    if disk[i]=="backup":
+        suf = " -."
+        for f in range(3):                  
+            print("              +-- "+backup_folder[f])
+            if f == 2: break
+            print("              |")
     last_sp= i == len(disk)-1
     if last_sp == True:
         print("  ")
     else: print("  |")
+
